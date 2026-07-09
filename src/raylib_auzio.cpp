3c7fe7ce5b872f12d6124dabe8ebd936a82735ef
nicbenji <172269214+nicbenji@users.noreply.github.com>
2026-07-09 18:54:06 +0200

    readme

--- src/raylib_auzio.cpp
+++ src/raylib_auzio.cpp
@@ -2,18 +2,16 @@
 #include "raylib.h"
 #include <unistd.h>
 
-global_var b32 global_running;
-
 i32 main(int argc, char **argv) {
-    global_running = true;
-
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(780, 540, "Auzio Editore");
+    InitWindow(800, 450, "Auzio Editore");
+
+    // TODO: query device capas?
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(BLACK);
         EndDrawing();
     }
 
