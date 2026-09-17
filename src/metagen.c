b4bdd5b5277b023aa8e90e8bb52cf03301473727
nicbenji <172269214+nicbenji@users.noreply.github.com>
2026-09-18 00:15:39 +0200

    refactor of lexing code

--- src/metagen.c
+++ src/metagen.c
@@ -1,5 +1,8 @@
 #include "base/base_inc.h"
 #include "base/base_inc.c"
+#include "base/base_strings.h"
+
+#define MAX_TOKENS 4096
 
 typedef enum {
     TokenKind_OpenParen,
@@ -15,12 +18,6 @@ typedef struct {
     TokenKind kind;
     Str8 str;
 } Token;
-
-// Does not filter out new-lines, only for operating on one line
-internal B32 mgen_is_whitespace(U8 ws) {
-    B32 result = (ws == ' ' || ws == '\t' || ws == '\r');
-    return result;
-}
 
 global_var Str8 keywords[] = {
     str8_lit("void"),
@@ -58,88 +55,62 @@ internal Str8 mgen_get_identifier(Str8 str) {
     return result;
 }
 
+internal TokenKind mgen_get_delim_token_kind(U8 c) {
+    switch (c) {
+        case ';': return TokenKind_Semicolon;
+        case ',': return TokenKind_Comma;
+        case '(': return TokenKind_OpenParen;
+        case ')': return TokenKind_CloseParen;
+        case '*': return TokenKind_Star;
+        UnreachableDefaultCase; // NOTE: maybe make this a human-readable error msg instead
+    }
+}
+
 
 internal int main_entrypoint(int argc, Str8 *argv) {
     MemoryArena *mgen_arena = mem_arena_default();
     Str8 in_file_funcs = file_read_all(mgen_arena, str8_lit("wl_client.signatures"));
     Assert(in_file_funcs.length > 0);
 
-    Str8List func_signatures = str8_split(mgen_arena, in_file_funcs, (U8 *)"\n", 1);
+    // prototype := return_type proc_name '(' parameters ')' ';'
+    // parameters := parameter {', ' parameter} | empty | 'void'
+    // parameter := type identifier
+    // type := base_type { '*' } ;
+    // base_type := 'struct' identifier | identifier ;
+    // proc_name := identifier
+    Token tokens[MAX_TOKENS]; // NOTE: maybe use dynamic array in future
+    U64 token_count = 0;
 
-    Str8ListNode *signature = func_signatures.first;
-    while (signature != 0) {
-        // prototype := return_type proc_name '(' parameters ')' ';'
-        // parameters := parameter {', ' parameter} | empty | 'void'
-        // parameter := type identifier
-        // type := base_type { '*' } ;
-        // base_type := 'struct' identifier | identifier ;
-        // proc_name := identifier
-        Token tokens[1024];
-        U64 token_count = 0;
+    U8 *at = in_file_funcs.begin;
+    U8 *one_past_last = at + in_file_funcs.length;
+    while (at < one_past_last) {
+        Str8 to_lex = str8_from_range(at, one_past_last);
+        if (char_is_whitespace(*at)) {
+            ++at;
+        } else if (mgen_is_identifier_begin(*at)) {
+            Token token = {0};
+            token.str = mgen_get_identifier(to_lex);
+            token.kind = mgen_is_keyword(token.str)
+                ? TokenKind_Keyword
+                : TokenKind_Identifier;
 
-        U8 *at = signature->str.begin;
-        U8 *one_past_last = at + signature->str.length;
-        for (; at < one_past_last;) {
-            Str8 to_lex = str8_from_range(at, one_past_last);
-            if (mgen_is_whitespace(*at)) {
-                ++at;
-            } else if (*at == ';') {
-                Token token = {
-                    .kind = TokenKind_Semicolon,
-                    .str = str8(at, 1)
-                };
-                tokens[token_count++] = token;
-                ++at;
-            } else if (*at == ',') {
-                Token token = {
-                    .kind = TokenKind_Comma,
-                    .str = str8(at, 1)
-                };
-                tokens[token_count++] = token;
-                ++at;
-            } else if (*at == '*') {
-                Token token = {
-                    .kind = TokenKind_Star,
-                    .str = str8(at, 1)
-                };
-                tokens[token_count++] = token;
-                ++at;
-            } else if (*at == '(') {
-                Token token = {
-                    .kind = TokenKind_OpenParen,
-                    .str = str8(at, 1)
-                };
-                tokens[token_count++] = token;
-                ++at;
-            } else if (*at == ')') {
-                Token token = {
-                    .kind = TokenKind_CloseParen,
-                    .str = str8(at, 1)
-                };
-                tokens[token_count++] = token;
-                ++at;
-            } else if (mgen_is_identifier_begin(*at)) {
-                Token token = {0};
-                token.str = mgen_get_identifier(to_lex);
-                token.kind = mgen_is_keyword(token.str)
-                    ? TokenKind_Keyword
-                    : TokenKind_Identifier;
+            Assert(token_count < 4096);
+            tokens[token_count++] = token;
+            at += token.str.length;
+        } else {
+            Token token = {0};
+            token.str = str8(at, 1);
+            token.kind = mgen_get_delim_token_kind(*at);
 
-                tokens[token_count++] = token;
-                at += token.str.length;
-            }
-
-
+            Assert(token_count < 4096);
+            tokens[token_count++] = token;
+            ++at;
         }
+    }
 
-        printf("Tokens:\n");
-        for (U64 i = 0; i < token_count; ++i) {
-            printf("Kind: %d, Txt: %.*s; ", tokens[i].kind, str8_vargs(tokens[i].str));
-        }
-        printf("\n");
-
-
-        signature = signature->next;
+    printf("Tokens:\n");
+    for (U64 i = 0; i < token_count; ++i) {
+        printf("Kind: %d, Txt: %.*s\n", tokens[i].kind, str8_vargs(tokens[i].str));
     }
 
     return 0;
